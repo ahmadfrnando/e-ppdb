a1cf93d340bb7f346765440cf6042646bc3df781
grace-manulang <gracemanulang19@gmail.com>
2024-07-25 06:54:44 +0700

    hapus report pdf diganti report excel

--- app/Filament/Resources/StudentResource/Pages/ListStudents.php
+++ app/Filament/Resources/StudentResource/Pages/ListStudents.php
@@ -9,35 +9,34 @@ use Filament\Forms\Components\Select;
 
 class ListStudents extends ListRecords
 {
-    protected static string $resource = StudentResource::class;
+  protected static string $resource = StudentResource::class;
 
-    protected function getHeaderActions(): array
-    {
-        return [
-            // Actions\CreateAction::make(),
-            Actions\Action::make('Export')
-            ->action(function() {
-                return redirect()->route('export.laporan-seleksi');
-            }),
-            Actions\Action::make('Reports')
-                ->icon('heroicon-o-printer')
-                ->form([
-                    Select::make('diterima')
-                        ->options([
-                            0 => 'Proses Seleksi',
-                            1 => 'Diterima',
-                            2 => 'Ditolak',
-                        ])
-                        ->required(),
-                ])
-                ->action(function (array $data) {
-                    // Eksekusi logika setelah form disubmit
-                    $diterima = $data['diterima'];
-                    // Redirect ke route yang ditentukan dengan parameter tanggal
-                    return redirect()->route('download.laporan-seleksi', [
-                        'diterima' => $diterima,
-                    ]);
-                }),
-        ];
-    }
+  protected function getHeaderActions(): array
+  {
+    return [
+      // Actions\CreateAction::make(),
+      Actions\Action::make('Reports')->action(function () {
+        return redirect()->route('export.laporan-seleksi');
+      }),
+    //   Actions\Action::make('Reports')
+    //     ->icon('heroicon-o-printer')
+    //     ->form([
+    //       Select::make('diterima')
+    //         ->options([
+    //           0 => 'Proses Seleksi',
+    //           1 => 'Diterima',
+    //           2 => 'Ditolak',
+    //         ])
+    //         ->required(),
+    //     ])
+    //     ->action(function (array $data) {
+    //       // Eksekusi logika setelah form disubmit
+    //       $diterima = $data['diterima'];
+    //       // Redirect ke route yang ditentukan dengan parameter tanggal
+    //       return redirect()->route('download.laporan-seleksi', [
+    //         'diterima' => $diterima,
+    //       ]);
+    //     }),
+    ];
+  }
 }
